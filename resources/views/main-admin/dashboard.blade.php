1278db1fe3e13fb36779f550dea0dd34772b34d5
Hasan Ajweh <152981163+hasanajweh@users.noreply.github.com>
2025-11-16 04:18:16 +0200

    Fix dashboard translation lookups

--- resources/views/main-admin/dashboard.blade.php
+++ resources/views/main-admin/dashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.school')
 
-@section('title', __('messages.dashboard'))
+@section('title', __('messages.dashboard.dashboard'))
 
 @section('content')
 <div class="container mx-auto px-4 py-6">
